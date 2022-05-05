de7c38d9d7c2d95c8d9e3a7d2452c083db91b29c
mohammad Ali <mohammadharb96170@gmail.com>
2022-05-05 23:10:45 +0300

    delete converter file

--- utilities/converter.js
+++
@@ -1,6 +0,0 @@
-export function convertDocToObject(doc){
-    doc._id = doc?._id.toString()
-    if(doc.createdAt)  doc.createdAt = doc.createdAt.toString()
-    if(doc.updatedAt)  doc.updatedAt = doc.updatedAt.toString() 
-    return doc
-}
