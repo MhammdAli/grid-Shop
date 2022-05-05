a438683e1b7e9b014b6d053f7614c0df5b939fc5
mohammad Ali <mohammadharb96170@gmail.com>
2022-05-05 22:41:29 +0300

    UPDATE mongodberror module

--- middlewares/errorMiddlewares.js
+++ middlewares/errorMiddlewares.js
@@ -1,3 +1,4 @@
+import multer from "multer"
 import nc from "next-connect" 
  
 export const handler = nc({
@@ -5,6 +6,12 @@ export const handler = nc({
         res.status(405).json({error : `${req.method} METHOD NOT ALOWED`})
     },
     onError : (err,req,res)=>{ 
-        res.status(err.status || 500,err.message)
+        if(err instanceof multer.MulterError){ 
+            res.status(400).json({
+                code : err.code, 
+                message : err.message
+            }) 
+        }else
+           res.status(err.status || 500,err.message)
     }
 })
