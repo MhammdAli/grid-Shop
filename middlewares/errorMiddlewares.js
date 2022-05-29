9826085953874048ece265c3174935c597883a2b
mohammad Ali <mohammadharb96170@gmail.com>
2022-05-29 20:36:18 +0300

    modify error Middlewares

--- middlewares/errorMiddlewares.js
+++ middlewares/errorMiddlewares.js
@@ -1,15 +1,21 @@
 import multer from "multer"
+import { isUploaderError } from "../lib/Uploader/uploaderErrors"
 
 export function NoMatchEndpoint(req,res){
     res.status(405).json({error : `${req.method} METHOD NOT ALOWED`})
 }
 
 export function errorHandler(err,req,res){ 
-    if(err instanceof multer.MulterError){ 
+    console.log( isUploaderError(err))
+    if(err instanceof multer.MulterError || isUploaderError(err)){   
         res.status(400).json({
-            code : err.code, 
-            message : err.message
+            name : err?.name,
+            message : err?.message
         }) 
-    }else
-       res.status(err.status || 500,err.message)
+    }else{ 
+        res.status(err?.status || 500)
+        .json({
+            message : err?.message
+        }) 
+    }
 }
